cc6dd23bb02a501c9fe2d58f412eba3dfbd20205
macbookair <macbookair@100.65.203.115>
2020-04-23 10:03:45 +0530

    Commit....

--- AssesmentDemo/src/com/TestBase.java
+++ AssesmentDemo/src/com/TestBase.java
@@ -1,5 +1,38 @@
 package com;
 
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestBase {
+	WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/Users/macbookair/Downloads/chromedriver");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.get("https://www.youtube.com/");
+		
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
